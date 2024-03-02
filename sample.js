7a945489d127e6cbbbe298e0ffd57d6ce6d93b16
venkatasurace <106607525+venkatasurace@users.noreply.github.com>
2024-03-02 15:29:07 +0530

    change 3 commit 3

---
+++ sample.js
@@ -0,0 +1,8 @@
+let array = [{name:"suresh",salary:50000},{name:"ashok" ,salary:30000},{name:"mahesh" ,salary:70000},{name:"rakesh",salary:20000},
+{name:"gana",salary:50000}]
+
+//first commit for sort
+array.sort((a, b) => a.salary - b.salary);
+console.log(array);
+
+//commit 2 commits for sort
